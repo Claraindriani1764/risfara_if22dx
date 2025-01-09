d6b3651b1631ce0e87c279a5973991b79aaa1630
Claraindriani1764 <claraindriani1764@gmail.com>
2025-01-09 23:25:34 +0700

    "clara: Menginstall daisyui dan membuat  theme di tainwild.config.ts"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,8 +11,39 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "merah": "#ff0000",
+        "kuning": "#ffcc00",
+        "hitam": {
+          "tua": "#000000",
+          "sedang": "#333333",
+          "muda": "#666666",
+        },
+        "putih": "#ffffff",
+        "biru": "#0000ff",
+      },
+      spacing: {
+        5 : "5px",
+        10 : "10px",
+        40 : "40px",
+        "20px" :"20px",
+        "5%": "5%",
+        "10%": "10%",
+        "15%": "15%",
+        "20%": "20%",
+        "25%": "25%",
+        "30%": "30%",
+        "35%": "35%",
+        "40%": "40%",
+        "45%": "45%",
+        "50%": "50%",
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dark", "retro"],
+  },
+};
+export default config;
