df84f09e565ade4ee13011999b2bd5f3ac1d8bd1
Faniptricia <fanipatricia03@gmail.com>
2025-01-11 20:29:20 +0700

    Fani: Menambahkan Initial Layout Menu

---
+++ app/menu/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import Link from "next/link";
+export default function Menu() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-putih p-[20px] relative">
+      <div className="absolute top-0 left-0 w-full h-full bg-gray-50 -z-20"></div>
+    </div>
+  );
+}
